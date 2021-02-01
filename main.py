e87ea9fbf465f319831bfe8b7e89fe21d802ea60
root <shanemmattner@gmail.com>
2021-02-01 01:27:56 +0000

    getting 1200 samples back, the samples on screen

--- main.py
+++ main.py
@@ -5,37 +5,23 @@ import ds1054z_cp as dscp
 
 #from ds1054z import DS1054Z 
 def main():
-    #instrument = DS1000Z('169.254.145.221')
-    #print(instrument.get_identification())
-    #instrument.stop()
-
-    # Grab the data from channel 1
-    #df = instrument.get_data()
-
 
     scope = dscp.DS1054Z('169.254.145.221')
     print("Connected to: ", scope.idn)
 
+    depth = scope.memory_depth_internal_total
+    print('Mem depth' + str(depth))
+    scope.stop()
     print("currently displayed channels: ", str(scope.displayed_channels))
-
-    t_test = scope.get_waveform_samples('CHAN1')
-    print(str(len(t_test)))
-    #print(str(len(rawdata)))
-    #data = np.frombuffer(rawdata, 'S')
-
-    #t_list = rawdata[11:].split(",")
-    #for i in t_list:
-     #   try:
-      #      float(i)
-       # except:
-        #    print("failed on: " + str(i))
-         #   continue
-    #[float(i) for i in t_list]
-    #print(str(rawdata))
-    #df = pd.DataFrame(t_list, columns=['voltage'])
-#    data = np.frombuffer(rawdata, 'B')
-    #print(str(len(df))) 
- 
+    scope.single()
+    print(str(scope.running))
+    #wait until the scope has stopped running
+    #TODO: make sure we don't sit here forever
+    while(scope.running):
+        continue
+    samples_list = scope.get_waveform_samples('CHAN1')
+    df = pd.DataFrame(samples_list, columns = ['samples'])
+    print(df)
  
 if __name__ == "__main__":
     main()
