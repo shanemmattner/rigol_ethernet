16ad7331cdb201e3fa2bf59aa42280b1f9e1009e
root <shanemmattner@gmail.com>
2021-02-04 00:48:07 +0000

    all 4 parts are triggering and logging

--- main.py
+++ main.py
@@ -17,19 +17,102 @@ def GPIO_setup():
     GPIO.setup(lock_pin, GPIO.OUT)
     GPIO.setup(unlock_pin, GPIO.OUT)
 
+def stall_lock_test():
+    scope = dscp.DS1054Z('169.254.145.221')
+    print("Connected to: ", scope.idn)
+    scope.stop()
+    print("currently displayed channels: ", str(scope.displayed_channels))
+    #make sure the the trigger source is the correct channel
+    scope.write(':TRIG:EDGE:SOUR CHAN3')
+    scope.write(':TRIG:EDGE:SLOP NEG')
+    
+    
+    
+    time.sleep(1)
+    scope.single()
+    GPIO.output(lock_pin, GPIO.HIGH)
+    time.sleep(0.5)
+    print("GPIO high")
+    GPIO.output(lock_pin, GPIO.LOW)
+    while(scope.running):
+        print("scope still running, lock stall")
+    scope.stop()
+
+    print("Getting sample data")
+    time_data = scope.waveform_time_values
+    df = pd.DataFrame(time_data, columns = ['TIME'])
+    chan4_data = scope.get_waveform_samples('CHAN4')
+    df.insert(1, 'CHAN4', chan4_data)
+    chan3_data = scope.get_waveform_samples('CHAN3')
+    df.insert(1, 'CHAN3', chan3_data)
+    chan2_data = scope.get_waveform_samples('CHAN2')
+    df.insert(1, 'CHAN2', chan2_data)
+    chan1_data = scope.get_waveform_samples('CHAN1')
+    df.insert(1, 'CHAN1', chan1_data)
+
+    print(df) 
+
+    conn = sql.connect('g3v2_motor_drive_signals.db')
+    df.to_sql('lock_stall', conn, if_exists='append')
+    conn.close()
+
+
+
+#Need to set the oscilloscope to trigger on the output going low
+def stall_unlock_test():
+    scope = dscp.DS1054Z('169.254.145.221')
+    scope.stop()
+
+    #make sure the the trigger source is the correct channel
+    scope.write(':TRIG:EDGE:SOUR CHAN4')
+    scope.write(':TRIG:EDGE:SLOP NEG')
+    scope.write(':TRIG:EDGE:LEV 1')
+
+    #scope.run()
+    time.sleep(1)
+    scope.single()
+    GPIO.output(unlock_pin, GPIO.HIGH)
+    time.sleep(0.5)
+    print("GPIO high")
+    GPIO.output(unlock_pin, GPIO.LOW)
+    while(scope.running):
+        print("scope still running, unlock stall")
+    scope.stop()
+
+    print("Getting sample data")
+    time_data = scope.waveform_time_values
+    df = pd.DataFrame(time_data, columns = ['TIME'])
+    chan4_data = scope.get_waveform_samples('CHAN4')
+    df.insert(1, 'CHAN4', chan4_data)
+    chan3_data = scope.get_waveform_samples('CHAN3')
+    df.insert(1, 'CHAN3', chan3_data)
+    chan2_data = scope.get_waveform_samples('CHAN2')
+    df.insert(1, 'CHAN2', chan2_data)
+    chan1_data = scope.get_waveform_samples('CHAN1')
+    df.insert(1, 'CHAN1', chan1_data)
+    
+    print(df) 
+
+    conn = sql.connect('g3v2_motor_drive_signals.db')
+    df.to_sql('unlock_stall', conn, if_exists='append')
+    conn.close()
+
+
+
 def inrush_lock_test():
    
     scope = dscp.DS1054Z('169.254.145.221')
-    print("Connected to: ", scope.idn)
-
     scope.stop()
-    print("currently displayed channels: ", str(scope.displayed_channels))
-    scope.run()
-    time.sleep(0.5)
-    scope.tforce()
+
+    scope.write(':TRIG:EDGE:SOUR CHAN3')
+    scope.write(':TRIG:EDGE:SLOP POS')
+    scope.write(':TRIG:EDGE:LEV 1')
+
+
+    scope.single()
+    time.sleep(1)
     GPIO.output(lock_pin, GPIO.HIGH)
     time.sleep(1)
-    print("GPIO high")
     GPIO.output(lock_pin, GPIO.LOW)
     scope.stop()
 
@@ -59,12 +142,14 @@ def inrush_unlock_test():
 
     scope.stop()
     print("currently displayed channels: ", str(scope.displayed_channels))
-    scope.run()
-    time.sleep(0.5)
-    scope.tforce()
+    scope.write(':TRIG:EDGE:SOUR CHAN4')
+    scope.write(':TRIG:EDGE:SLOP POS')
+    scope.write(':TRIG:EDGE:LEV 1')
+
+    scope.single()
+    time.sleep(1)
     GPIO.output(unlock_pin, GPIO.HIGH)
     time.sleep(1)
-    print("GPIO high")
     GPIO.output(unlock_pin, GPIO.LOW)
     scope.stop()
 
@@ -97,31 +182,15 @@ def main():
     GPIO_setup()
     
     #inrush_lock_test()
-    for x in range(5):
-        time.sleep(5)
+    for x in range(2):
         inrush_lock_test()
-        time.sleep(5)
+        time.sleep(3)
+        stall_lock_test()
+        time.sleep(3)
         inrush_unlock_test()
-    #append test number
-
-    #get time data
-    
-   # print("Scope not running anymore")
-   # print(str(scope.running))
-   # scope.stop()
-   # print("Getting sample data")
-   # samples_list = scope.get_waveform_samples('CHAN1')
-   # df = pd.DataFrame(samples_list, columns = ['samples'])
-   # df['time']=scope.waveform_time_values
-   # print(df) 
-
-    #conn = sql.connect('triac.db')
-    #tables = conn.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    #table_list = []
-    #for name in tables:
-    #    table_list.append(name[0])
-    #print(table_list)
-    #df.to_sql('traic', conn, if_exists='replace')
+        time.sleep(3)
+        stall_unlock_test()
+        time.sleep(3)
  
 if __name__ == "__main__":
     main()
