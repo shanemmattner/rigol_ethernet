17c0955b9c834b7095655b105dd1a2932df2eb37
root <shanemmattner@gmail.com>
2021-02-04 22:33:36 +0000

    2 buttons working

--- not_test.py
+++ not_test.py
@@ -1,35 +1,53 @@
+
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+import dash_core_components as dcc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+app.layout = html.Div([
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit'),
+    html.Button('btn1', id='add', n_clicks=0),
+    html.Button('btn2', id='subtract', n_clicks=0),
+    dcc.Interval(
+        id='interval-component',
+        interval=1*1000, # in milliseconds
+        n_intervals=0
+        ),
+    html.Div(id='null_1'),
+    html.Div(id='null_2'),
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    
 ])
 
+
+@app.callback(
+    dash.dependencies.Output('container-button-basic', 'children'),
+    [dash.dependencies.Input('add', 'n_clicks')])
+def update_output(n_clicks):
+    return 'The button has been clicked {} times'.format(
+        n_clicks
+    )
+
+@app.callback(
+    dash.dependencies.Output('null_2', 'children'),
+    [dash.dependencies.Input('subtract', 'n_clicks')])
+def update_output(n_clicks):
+    return 'The button has been clicked {} times'.format(
+        n_clicks
+    )
+
+@app.callback(dash.dependencies.Output('null_1', 'children'),
+              dash.dependencies.Input('interval-component', 'n_intervals'))
+def update_metrics(n):
+
+    return [
+        n
+    ]
+
+
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0')
+    app.run_server(host='0.0.0.0', debug=True)
